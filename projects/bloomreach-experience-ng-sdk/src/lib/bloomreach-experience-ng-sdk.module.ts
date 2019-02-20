ff548f7975079c3b4629e1a7cf6295c103518a71
Matt Habegger <matt.habegger@capitalone.com>
2019-02-20 12:27:31 -0800

    removed httpclientmodule import to fix prerender

--- projects/bloomreach-experience-ng-sdk/src/lib/bloomreach-experience-ng-sdk.module.ts
+++ projects/bloomreach-experience-ng-sdk/src/lib/bloomreach-experience-ng-sdk.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { ApiUrlsService } from './services/api-urls.service';
@@ -33,7 +32,6 @@ import { InitializeSdkService } from './services/initialize-sdk.service';
   entryComponents: [ UndefinedComponent ],
   imports: [
     CommonModule,
-    HttpClientModule,
     RouterModule,
   ],
   providers: [ApiUrlsService, ComponentMappingsService, ImageUrlService, InitializeSdkService, PageModelService, RequestContextService],
